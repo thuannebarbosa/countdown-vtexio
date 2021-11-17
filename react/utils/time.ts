b1a4c7777babfc9107f67d2f42018efb4f4b270b
Thuanne Barbosa <barbosa.thuanne@gmail.com>
2021-11-17 14:47:11 -0300

    Update time.ts

--- react/utils/time.ts
+++ react/utils/time.ts
@@ -8,15 +8,11 @@ const fillWithZero = (digits: number, number: number): string => {
 }
 
 export const parseTimeRemaining = (totalSeconds: number): TimeSplit => {
-  const days = Math.floor(totalSeconds/86400)
-  totalSeconds = totalSeconds % 86400 
 
-  const hours = Math.floor( totalSeconds / 3600)
-  totalSeconds = totalSeconds % 3600
-
-  const minutes = Math.floor( totalSeconds / 60  )
-
-  const seconds = Math.floor( totalSeconds % 60 ) 
+  var days = Math.floor(totalSeconds / 86400);
+  var hours = Math.floor((totalSeconds - (days * 86400)) / 3600);
+  var minutes = Math.floor((totalSeconds - (days * 86400) - (hours * 3600)) / 60);
+  var seconds = Math.floor((totalSeconds - (days * 86400) - (hours * 3600) - (minutes * 60)));
   
 
   
@@ -30,20 +26,20 @@ export const parseTimeRemaining = (totalSeconds: number): TimeSplit => {
 }
 
 /**
-
  * @param targetDate ISOString for the date that the countdown will expire
  * @param dispatchFn A function that updates the state of the component
- */
+**/
 export const tick = (
   targetDate: string,
   dispatchFn: React.Dispatch<React.SetStateAction<TimeSplit>>
 ) => {
   const ONE_SECOND_IN_MILLIS = 1000
-  const finalDate = new Date(targetDate) //2021-11-26
-  const now = new Date() // 
+  const finalDate = new Date(targetDate)
+  const now = new Date() 
 
+  
   const secondsLeft =
-    (finalDate.getTime() - now.getTime()) / ONE_SECOND_IN_MILLIS
+    (finalDate.getTime() / 1000) - ( now.getTime() / 1000)
 
   setTimeout(() => {
     dispatchFn(parseTimeRemaining(secondsLeft))
@@ -53,8 +49,6 @@ export const tick = (
 
 export const getTwoDaysFromNow = () => {
   const today = new Date()
-
   today.setDate(today.getDate() + 2)
-
   return today.toISOString()
 }
